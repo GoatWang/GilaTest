11706ce71432cc53c381cd18d8bc95337d3fda0c
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-06-06 13:08:03 +0800

    first

---
+++ Q2_1.py
@@ -0,0 +1,69 @@
+from collections import Counter
+import math
+import operator
+
+def ngram_probs(filename='raw_sentences.txt'):
+    bigram = []
+    trigram = []
+    with open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            texts = [t.lower() for t in line.split()]
+            for i in range(len(texts)-1):
+                bigram.append("#$".join(texts[i:i+2]))
+            for i in range(len(texts)-2):
+                trigram.append("#$".join(texts[i:i+3]))
+
+    bigram_probs = dict([(tuple(i.split("#$")), v) for i, v in dict(Counter(bigram)).items()])
+    trigram_probs = dict([(tuple(i.split("#$")), v) for i, v in dict(Counter(trigram)).items()])
+    return bigram_probs, trigram_probs
+cnt2, cnt3 = ngram_probs()
+print(cnt2[('we', 'are')])
+
+def prob3(bigram, cnt2=cnt2, cnt3=cnt3):
+    cnt2_sum = sum(cnt2.values())
+    cnt3_sum = sum(cnt3.values())
+    cnt2_prob = cnt2[bigram]/cnt2_sum
+
+    prob = {}
+    for i, v in cnt3.items():
+        if i[0] == bigram[0] and i[1] == bigram[1]:
+            prob[i[2]] = math.log((v/cnt3_sum)/cnt2_prob)
+    
+    return prob
+p = prob3(('we', 'are'))
+print(p['family'])
+
+def predict_top_beam_size(beam_size, bigram):
+    curent_prob3 = prob3(tuple(bigram))
+    sorted_prob3 = sorted(curent_prob3.items(), key=operator.itemgetter(1), reverse=True)[:beam_size]
+    return [t for t, v in sorted_prob3]
+
+
+def predict_max(starting, cnt2=cnt2, cnt3=cnt3):
+    list_of_words = list(starting)
+    while len(list_of_words) < 15 and list_of_words[-1] != '.':
+        curent_prob3 = prob3(tuple(list_of_words[-2:]))
+        next_word = max(curent_prob3.items(), key=operator.itemgetter(1))[0]
+        list_of_words.append(next_word)
+    return list_of_words
+
+sent = predict_max(('we', 'are'))
+assert sent[-1] == '.' or len(sent) <= 15
+print(' '.join(sent))
+
+
+def next_predict():
+    
+
+def predict_beam(bigram, beam_size=4, sent_length=10, cnt2=cnt2, cnt3=cnt3):
+    list_of_words = list(bigram)
+    while len(list_of_words) < 15 and list_of_words[-1] != '.':
+        curent_prob3 = prob3(tuple(list_of_words[-2:]))
+        predict_top_beam_size
+
+        list_of_words.append(next_word)
+    return list_of_sentence
+
+for sent in predict_beam(('we', 'are')):
+    assert sent[-1] == '.' or len(sent) <10:
+    print(' '.join(sent))
---
+++ test.md
@@ -0,0 +1,17 @@
+# Q1-1
+    ![answer](34470485_1740773745999083_7221755592874917888_n.jpg)
+
+# Q1-2
+1. F1-score: 
+    - 如果拿到一個資料集，其中發現絕大多數的資料都是某一個類別，那麼一旦機器全部猜測那個類別，將可以得到相對較高的precision。
+2. why not binary classification:
+    - 因為，將導致activate fincation不平滑，進一步使得輸出結果非常極端，一但被分成特定類別，後面的back prop就很難修正。
+3. bais and variance:
+    - bais指得是演算法本身的假設錯誤逤導致的偏差，將導致model無法真正fit到正確的位置。variance指得是model對於資料的變動極度敏感而導致的偏差，當model過度fit在某一些資料上時，將會產勝這樣的問題。
+4. 
+5. onehot:
+    - 指將類別資料轉換成0與1組成的向量。如[0,1,2] => [[1,0,0], [0,1,0], [0,0,1]]
+6. overfitting:
+    - Regularization: L1 or L2 norm，讓輸出結果正規劃。
+    - dropout: 讓部份的node失去功效。雖然有看到論文說這是無效的作法，不過根據過去的經驗，還是有避免overfitting的功效。
+    - 區分train test set: 讓部分資料不要放入model進行訓練，最後用test set來檢測是否有overfitting。
